644ca24c47df9f3cc4a43323a399b49b921a33b6
miyazawa <ryosukemiyazawa@gmail.com>
2018-04-21 19:31:11 +0900

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,9 @@
 const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
-const cleanCSS = require('gulp-clean-css');
-const rename = require('gulp-rename')
 
 var paths = "public/**/*.html"
 var css_path = "layouts/partials/*.css"
 var css_min_path = "layouts/partials"
-
-gulp.task('minify-css', () => {
-  return gulp.src(css_path)
-    .pipe(cleanCSS({}))
-    .pipe(rename({extname: '.min.css'}))
-    .pipe(gulp.dest(css_min_path))
-});
 
 gulp.task('minify-html', () => {
   return gulp.src(paths)
